65dcdf0397dc33442db9a774243362646149c291
HaroldCV <70811385+HaroldCV@users.noreply.github.com>
2022-04-05 18:39:57 -0500

    Update double.h

--- double.h
+++ double.h
@@ -3,7 +3,6 @@
 #include <iostream>
 #include "list.h"
 
-// TODO: Implement all methods
 template <typename T>
 class DoubleList : public List<T> {
     private:
@@ -11,11 +10,46 @@ class DoubleList : public List<T> {
         Node<T>* tail;
         int nodes;
     public:
-        DoubleList() : List<T>() {}
+    
+    DoubleList() : List<T>() { 
+             this->head = nullptr; 
+             this->tail = nullptr; 
+             nodes =0; 
+         } 
 
         ~DoubleList(){ 
-         	// TODO            
+         	    clear(); 
+              delete head; 
+              delete tail; 
         }
+    T front() override 
+         { 
+             return head->data; 
+         } 
+  
+         T back() override 
+         { 
+             return tail->data; 
+         } 
+    
+    
+    void push_front(T value) override 
+         { 
+             if (is_empty()){ 
+                 head->data = value; 
+                 head->next = NULL; 
+                 head->prev = NULL; 
+                 nodes++; 
+             } 
+             Node<T>* temp = new Node<T>(value); 
+             temp->next = head; 
+             temp->data = value; 
+             head->prev = temp; 
+             tail = head; 
+             head = temp; 
+             head->prev = NULL; 
+             tail->next = NULL; 
+         } 
         
 };
 #endif
