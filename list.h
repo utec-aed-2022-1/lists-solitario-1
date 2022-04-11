ff65a2c5b242563c5df7811672c7e73a3c7505a3
HaroldCV <70811385+HaroldCV@users.noreply.github.com>
2022-04-11 15:54:49 -0500

    Update list.h

--- list.h
+++ list.h
@@ -23,8 +23,8 @@ class List {
         virtual void push_back(T) = 0;
         virtual T pop_front() = 0;
         virtual T pop_back() = 0;
-        virtual T insert(T, int) = 0;
-        virtual bool remove(int) = 0;
+        virtual void insert(T, int) = 0;
+        virtual void remove(int) = 0;
         virtual T& operator[](int) = 0;//debe ser declarado en cada clase hija
         virtual bool is_empty() = 0;
         virtual int size() = 0;
